0f21d94351563b877e53a0ad62066f4d47146521
Serphentas <m.danai@bluewin.ch>
2017-08-18 14:45:55 +0200

    asfafdtghw

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -5,7 +5,7 @@
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     
-    <title>Lanbuddy</title>
+    <title>Lanbuddy caca</title>
     
     <link rel="stylesheet" type="text/css" href="/css/foundation.min.css">
     <link rel="stylesheet" type="text/css" href="/css/style.css">
